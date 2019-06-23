805e102665aacfd9cdfeb64fe4750f55d6b799ec
franciscovillaescusa <villaescusa.francisco@gmail.com>
2019-06-23 10:39:56 -0400

    updated variables names

--- NN_Pk2.py
+++ NN_Pk2.py
@@ -36,28 +36,29 @@ class Model(nn.Module):
         return out
     
 # create the data to train the network
-def dataset(k, Nk, kpivot, batch_size, predict_gamma):
+def dataset(k, Nk, kpivot, batch_size, predict_C):
     data  = np.empty((batch_size, k.shape[0]), dtype=np.float32)
-    if predict_gamma:  label = np.empty((batch_size,3), dtype=np.float32)
-    else:              label = np.empty((batch_size,2), dtype=np.float32)
+    if predict_C:  label = np.empty((batch_size,3), dtype=np.float32)
+    else:          label = np.empty((batch_size,2), dtype=np.float32)
     indexes = np.where(k>kpivot)[0]
     i = 0
     while(i<batch_size):
-        alpha  = (9.9*np.random.random()+0.1)
-        beta   = -1.0 + 1.0*np.random.random()
-        gamma  = -0.5 + np.random.random()
-        Pk     = alpha*k**beta
+        A  = (9.9*np.random.random()+0.1)
+        B  = -1.0 + 1.0*np.random.random()
+        C  = -0.5 + np.random.random()
+        Pk = A*k**B
         if len(indexes)>0:
-            A      = Pk[indexes[0]]/k[indexes[0]]**gamma
-            Pk[indexes] = A*k[indexes]**gamma
-        dPk    = np.sqrt(Pk**2/Nk)
-        Pk     = np.random.normal(loc=Pk, scale=dPk)
-        if np.any(Pk<0):
-            continue
+            A_value = Pk[indexes[0]]/k[indexes[0]]**C
+            Pk[indexes] = A_value*k[indexes]**C
+        dPk = np.sqrt(Pk**2/Nk)
+        Pk  = np.random.normal(loc=Pk, scale=dPk)
+        #if np.any(Pk<0):
+        #    print 'bad'
+        #    continue
         data[i] = np.log10(Pk)
         #data[i] = Pk
-        if predict_gamma:  label[i] = [alpha, beta, gamma]
-        else:              label[i] = [alpha, beta]
+        if predict_C:  label[i] = [A, B, C]
+        else:          label[i] = [A, B]
         i += 1
     return torch.tensor(data),torch.tensor(label)
 
@@ -72,30 +73,30 @@ hidden2 = 50
 hidden3 = 50
 hidden4 = 50
 
-predict_gamma = False
+predict_C = False
 
-epochs           = 40000
+epochs           = 40
 batch_size_train = 16
-batch_size_valid = 64*5
+batch_size_valid = 64*10
 batch_size_test  = 64*100
 batches          = 100
 
 plot_results = False
 
 suffix = '50x50x50_40000_5000-10000-15000_20000_kpivot=2.0_no-gamma'
-fout = 'results/results_%s.txt'%suffix
+fout   = 'results/results_%s.txt'%suffix
 #######################################################################################
 
 # find the numbers that each cpu will work with
 numbers = np.where(np.arange(len(kmaxs))%nprocs==myrank)[0]
 
 # define the arrays containing the error on each parameter
-dalpha   = np.zeros(len(kmaxs), dtype=np.float64)
-dalpha_t = np.zeros(len(kmaxs), dtype=np.float64)
-dbeta    = np.zeros(len(kmaxs), dtype=np.float64)
-dbeta_t  = np.zeros(len(kmaxs), dtype=np.float64)
-dgamma   = np.zeros(len(kmaxs), dtype=np.float64)
-dgamma_t = np.zeros(len(kmaxs), dtype=np.float64)
+dA   = np.zeros(len(kmaxs), dtype=np.float64)
+dA_t = np.zeros(len(kmaxs), dtype=np.float64)
+dB   = np.zeros(len(kmaxs), dtype=np.float64)
+dB_t = np.zeros(len(kmaxs), dtype=np.float64)
+dC   = np.zeros(len(kmaxs), dtype=np.float64)
+dC_t = np.zeros(len(kmaxs), dtype=np.float64)
 
 # do a loop over the different kmax
 for l in numbers:
@@ -106,21 +107,15 @@ for l in numbers:
     # find the fundamental frequency, the number of bins up to kmax and the k-array
     kF     = kmin
     k_bins = int((kmax-kmin)/kF)
-    k      = np.arange(1,k_bins+2)*kF
+    k      = np.arange(2,k_bins+2)*kF
     Nk     = 4.0*np.pi*k**2*kF/kF**3  #number of modes in each k-bin
     
     # get a validation dataset
-    valid_data, valid_label = dataset(k, Nk, kpivot, batch_size_valid, predict_gamma)
-    #data = np.empty((valid_data.shape[1], valid_data.shape[0]+1))
-    #data[:,0] = np.log10(k)
-    #for i in xrange(valid_data.shape[0]):
-    #    data[:,i+1] = valid_data[i].numpy()
-    #np.savetxt('borrar.txt', data)
-
+    valid_data, valid_label = dataset(k, Nk, kpivot, batch_size_valid, predict_C)
     
     # define the network and loss
-    if predict_gamma:  last_layer = 3
-    else:              last_layer = 2
+    if predict_C:  last_layer = 3
+    else:          last_layer = 2
     net = Model(k.shape[0], hidden1, hidden2, hidden3, hidden4, last_layer)
     loss_func = nn.MSELoss()
 
@@ -180,7 +175,7 @@ for l in numbers:
         total_loss = 0
         for batch in xrange(batches):
     
-            data, label = dataset(k, Nk, kpivot, batch_size_train, predict_gamma)
+            data, label = dataset(k, Nk, kpivot, batch_size_train, predict_C)
             pred = net(data)
             loss = loss_func(pred, label)
             optimizer.zero_grad()
@@ -212,7 +207,7 @@ for l in numbers:
 
 
     ###### evaluate the performance of the model ######
-    test_data, test_label = dataset(k, Nk, kpivot, batch_size_test, predict_gamma)
+    test_data, test_label = dataset(k, Nk, kpivot, batch_size_test, predict_C)
 
     net = Model(k.shape[0],hidden1,hidden2,hidden3,hidden4,last_layer)
     net.load_state_dict(torch.load(fmodel))
@@ -221,24 +216,24 @@ for l in numbers:
     pred = net(test_data)
     print 'kmax = %.2f'%kmax
     
-    dalpha[l] = np.mean(((pred[:,0]-test_label[:,0])**2).detach().numpy())
-    print 'error alpha = %.3e'%dalpha[l]
+    dA[l] = np.mean(((pred[:,0]-test_label[:,0])**2).detach().numpy())
+    print 'error A = %.3e'%dA[l]
         
-    dbeta[l]  = np.mean(((pred[:,1]-test_label[:,1])**2).detach().numpy())
-    print 'error beta  = %.3e'%dbeta[l]
+    dB[l] = np.mean(((pred[:,1]-test_label[:,1])**2).detach().numpy())
+    print 'error B = %.3e'%dB[l]
         
-    if predict_gamma:
-        dgamma[l] = np.mean(((pred[:,2]-test_label[:,2])**2).detach().numpy())        
-        print 'error gamma = %.3e'%dgamma[l]
+    if predict_C:
+        dC[l] = np.mean(((pred[:,2]-test_label[:,2])**2).detach().numpy())        
+        print 'error C = %.3e'%dC[l]
     ###################################################
 
 # reduce the results
-comm.Reduce(dalpha, dalpha_t, root=0)
-comm.Reduce(dbeta,  dbeta_t,  root=0)
-comm.Reduce(dgamma, dgamma_t, root=0)
+comm.Reduce(dA, dA_t, root=0)
+comm.Reduce(dB, dB_t, root=0)
+comm.Reduce(dC, dC_t, root=0)
     
 # save results to file
 if myrank>0:  sys.exit()
-if predict_gamma:  np.savetxt(fout, np.transpose([kmaxs, dalpha_t, dbeta_t, dgamma_t]))
-else:              np.savetxt(fout, np.transpose([kmaxs, dalpha_t, dbeta_t]))
+if predict_C:  np.savetxt(fout, np.transpose([kmaxs, dA_t, dB_t, dC_t]))
+else:          np.savetxt(fout, np.transpose([kmaxs, dA_t, dB_t]))
 
